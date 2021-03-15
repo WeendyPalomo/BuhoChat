a0c29498ac0bbd59532bbe0d0a639af16b0e8828
Juan Bolaños <juan.bolanos@epn.edu.ec>
2021-03-14 21:49:49 -0500

    users add incompleto

--- buhochat-app/src/components/ChatList.js
+++ buhochat-app/src/components/ChatList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { List, Avatar } from "antd";
-import { db } from "../firebase/index";
+import { db, auth } from "../firebase/index";
 import { Button, Tooltip } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -10,38 +10,66 @@ const ChatList = () => {
   const [users, setUsers] = useState([]);
   const [num, setNum] = useState();
 
-  useEffect(() => {
-    const usersRef = db.ref("users/");
-    usersRef.on("value", (snapshot) => {
-      const users = snapshot.val();
-      console.log("users", users);
-      console.log("usersCount", snapshot.numChildren());
-    });
-  }, []);
+  useEffect(() => {}, []);
 
-  useEffect(() => {
-    console.log("fuera", num);
-    /*
-    db.ref("/users")
-      .orderByKey()
-      .limitToFirst(3)
+  async function getAnotherUser() {
+    const randomValue = 0;
+    const newUid = 0;
+    const currentUser = auth.currentUser;
+    const name = 0,
+      uid = 0;
+    if (currentUser != null) {
+      name = currentUser.displayName;
+      uid = currentUser.uid;
+    } else {
+      console.log("No hay usuario");
+    }
+
+    const random = await db
+      .ref("/users")
       .once("value")
-      .then((users) => console.log(Object.keys(users)));*/
-  }, [num]);
+      .then((snapshot) => getRandomInt(1, snapshot.numChildren()));
+
+    random.then((num) => {
+      //setNum(num);
+      randomValue = num;
+      console.log("dentro", num);
+    });
+
+    //console.log("randomvalue", randomValue);
+
+    await db.ref(`userschats/${randomValue}`).once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        console.log("snapvalid", snapshot.val().userid);
+        newUid = snapshot.val().userid;
+      } else {
+        console.log("No data available");
+      }
+    });
+    console.log("newnewuid", newUid);
+    console.log("uid", uid);
+
+    console.log("return uid", newUid);
+    return newUid;
+  }
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
   const hadleAddUserChat = () => {
-    const random = db
-      .ref("/users")
-      .once("value")
-      .then((snapshot) => getRandomInt(1, snapshot.numChildren()));
-
-    random.then((num) => {
-      setNum(num);
-      console.log("dentro", num);
+    const newUid = getAnotherUser();
+    console.log("definitiveuid", newUid);
+    db.ref(`users/${newUid}`).once("value", (snapshot) => {
+      const newUser = {
+        email: snapshot.val().email,
+        lastname: snapshot.val().lastname,
+        name: snapshot.val().name,
+        nickname: snapshot.val().nickname,
+      };
+      setUsers((prevState) => {
+        return [...prevState, newUser];
+      });
     });
   };
 
@@ -65,7 +93,7 @@ const ChatList = () => {
               avatar={
                 <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
               }
-              title={<a href="https://ant.design">{item.title}</a>}
+              title={<a href="https://ant.design">{item.nickname}</a>}
               description="Ant Design, a design language for background applications, is refined by Ant UED Team"
             />
           </List.Item>
